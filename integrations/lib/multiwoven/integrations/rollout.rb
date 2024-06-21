2f2614a28a0907c65f576e98642b1a0632f1d171
TivonB-AI2 <124182151+TivonB-AI2@users.noreply.github.com>
2024-06-21 08:05:54 -0400

    Resolve conflict in cherry-pick of b9f6d3c34eebf79d49991eb0b50bf5649b07245b and change the commit message

--- integrations/lib/multiwoven/integrations/rollout.rb
+++ integrations/lib/multiwoven/integrations/rollout.rb
@@ -2,7 +2,11 @@
 
 module Multiwoven
   module Integrations
+<<<<<<< HEAD
     VERSION = "0.1.75"
+=======
+    VERSION = "0.3.0"
+>>>>>>> b9f6d3c3 (feat(CE): Add MariaDB source connector (#215))
 
     ENABLED_SOURCES = %w[
       Snowflake
@@ -13,6 +17,11 @@ module Multiwoven
       SalesforceConsumerGoodsCloud
       AwsAthena
       Clickhouse
+<<<<<<< HEAD
+=======
+      AmazonS3
+      MariaDB
+>>>>>>> b9f6d3c3 (feat(CE): Add MariaDB source connector (#215))
     ].freeze
 
     ENABLED_DESTINATIONS = %w[

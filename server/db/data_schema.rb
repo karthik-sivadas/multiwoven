2274675b7ebce1c5040beaf3f47f2efcfae29d87
afthab vp <afthab@multiwoven.com>
2024-06-26 21:02:00 +0530

    Resolve conflict in cherry-pick of 0645849ce7cf5d7373250caad7c687cd30cc7fa7 and change the commit message

--- server/db/data_schema.rb
+++ server/db/data_schema.rb
@@ -1,3 +1,7 @@
 # frozen_string_literal: true
 
+<<<<<<< HEAD
 DataMigrate::Data.define(version: 20_240_524_141_637)
+=======
+DataMigrate::Data.define(version: 20_240_626_120_511)
+>>>>>>> 0645849c (chore(CE): pundit policy at role permissions level (#241))

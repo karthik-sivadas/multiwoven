3b09beb75336ddc86fd54036e4fb498321fceacd
afthab vp <afthab@multiwoven.com>
2024-07-02 16:46:24 +0530

    Resolve conflict in cherry-pick of db66ff397495c9a4693e11525dfbe95587a29129 and change the commit message

--- server/db/data_schema.rb
+++ server/db/data_schema.rb
@@ -1,3 +1,7 @@
 # frozen_string_literal: true
 
+<<<<<<< HEAD
 DataMigrate::Data.define(version: 20_240_524_141_637)
+=======
+DataMigrate::Data.define(version: 20_240_702_095_541)
+>>>>>>> db66ff39 (chore(CE): Update Role Descriptions (#261))
